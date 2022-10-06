42f67cb3d37b8e8742863ab878b5d173a128a157
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2022-10-07 04:23:44 +0530

    Add files via upload

---
+++ employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emplyeewageapp
+{
+    internal class employee
+    {
+        public static void Empattendance()
+        {
+            //Constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empcheck = random.Next(0, 2);
+            if (empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
